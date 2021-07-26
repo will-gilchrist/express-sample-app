4ff135f3a63686f930de92fb7f6f3faa1d13f24f
will-gilchrist <w.gilchrist98@gmail.com>
2021-07-26 12:51:48 +0100

    sample trigger commit

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const usersRouter = require('./routes/users');
 
 const app = express();
 
+// sample commit
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
